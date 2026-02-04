7ab17e6e1fb91bfa3a45df5b956fdb95560c47ae
Ariel Fu <xinyuf3@uw.edu>
2026-02-04 02:38:05 -0800

    set up canvas for HWK #4. A

--- sketches/sketch2.js
+++ sketches/sketch2.js
@@ -1,14 +1,27 @@
 // Instance-mode sketch for tab 2
+//  HWK #4. A
 registerSketch('sk2', function (p) {
   p.setup = function () {
-    p.createCanvas(p.windowWidth, p.windowHeight);
+    p.createCanvas(800, 500);
   };
+
+  let bgImg;
+  let ballImg;
+  let racketImg;
+  p.preload = function () {
+    bgImg = p.loadImage("images/tennis-court-background.avif");
+    ballImg = p.loadImage("images/tennis_ball_cartoon.png");
+    racketImg = p.loadImage("images/upright_tennis_racket.png");
+  };
+
   p.draw = function () {
-    p.background(220);
+    p.image(bgImg, 0, 0, p.width, p.height);
     p.fill(100, 150, 240);
     p.textSize(32);
     p.textAlign(p.CENTER, p.CENTER);
-    p.text('HWK #4. A', p.width / 2, p.height / 2);
   };
   p.windowResized = function () { p.resizeCanvas(p.windowWidth, p.windowHeight); };
 });
+
+
+//  HWK #4. A
