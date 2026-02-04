0480f91e0a66063fcc16135840b0d28401030a43
Ariel Fu <xinyuf3@uw.edu>
2026-02-04 03:19:35 -0800

    replace the basic minute hand with tennis racket

--- sketches/sketch2.js
+++ sketches/sketch2.js
@@ -47,9 +47,6 @@ registerSketch('sk2', function (p) {
     let m = p.minute();
     let s = p.second();
 
-    // Center
-    // p.translate(p.width / 2, p.height / 2);
-
     // second hand
     p.push();
     p.stroke(225);
@@ -60,10 +57,17 @@ registerSketch('sk2', function (p) {
 
     // minute hand
     p.push();
-    p.stroke(255);
-    p.strokeWeight(3);
     p.rotate(p.map(m, 0, 60, 0, 360));
-    p.line(0, 0, 0, -R * 0.6);
+    p.imageMode(p.CENTER);
+
+    // scale factor (try 0.35–0.6)
+    let scale = (R * 0.8) / racketImg.height;
+    p.scale(scale);
+
+    // after scaling, use image’s real pixel size
+    p.image(racketImg, 0, -racketImg.height * 0.4, racketImg.width, racketImg.height);
+
+    p.imageMode(p.CORNER);
     p.pop();
 
     // hour hand
