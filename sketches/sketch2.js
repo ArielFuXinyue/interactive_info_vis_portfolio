955d76b13aaf40eb51a38a06cce08a585c4f61c1
Ariel Fu <xinyuf3@uw.edu>
2026-02-04 02:55:42 -0800

    draw clock face base circles and changed background

--- sketches/sketch2.js
+++ sketches/sketch2.js
@@ -5,23 +5,36 @@ registerSketch('sk2', function (p) {
     p.createCanvas(800, 500);
   };
 
-  let bgImg;
+  // let bgImg;
   let ballImg;
   let racketImg;
   p.preload = function () {
-    bgImg = p.loadImage("images/tennis-court-background.avif");
+    // bgImg = p.loadImage("images/tennis-court-background.avif");
     ballImg = p.loadImage("images/tennis_ball_cartoon.png");
     racketImg = p.loadImage("images/upright_tennis_racket.png");
   };
 
   p.draw = function () {
-    p.image(bgImg, 0, 0, p.width, p.height);
-    p.fill(100, 150, 240);
-    p.textSize(32);
-    p.textAlign(p.CENTER, p.CENTER);
+    // p.image(bgImg, 0, 0, p.width, p.height);
+    p.background(100, 180, 250);
+    p.clockFace();
   };
-  p.windowResized = function () { p.resizeCanvas(p.windowWidth, p.windowHeight); };
+
+  p.clockFace = function () {
+    p.push();
+    p.translate(p.width / 2, p.height / 2);
+
+    const R = p.min(p.width, p.height) * 0.45;   // outer radius
+    p.noStroke();
+    p.fill(58, 80, 105, 220);
+    p.ellipse(0, 0, R * 2 + 20, R * 2 + 20);
+
+    p.fill(81,132,183, 180);
+    p.ellipse(0, 0, R * 2, R * 2);
+    p.pop();
+  }
+
+
+  p.windowResized = function () { };
 });
 
-
-//  HWK #4. A
